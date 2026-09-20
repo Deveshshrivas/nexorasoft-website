af8f5f4a1716409936bd5ec75646c1073e5e5c8a
Devesh Shrivas <114324384+deveshshrivas@users.noreply.github.com>
2026-09-20 18:18:39 +0530

    Fix syntax error in server.js

--- server/server.js
+++ server/server.js
@@ -49,10 +49,11 @@ mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected successfully');
     app.listen(PORT, () => {
-      console.log(Server running on port: );
+      console.log('Server running on port: ' + PORT);
     });
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
     process.exit(1);
   });
+
